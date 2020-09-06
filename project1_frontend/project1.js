61e2953214bdca7abcb2c77fae6acb03778bdf8f
Jasmine Edison <bigbluedanube@gmail.com>
2020-09-06 14:28:24 -0400

    working on JavaScript file. STILL INCOMPLETE.

--- project1_frontend/project1.js
+++ project1_frontend/project1.js
@@ -21,10 +21,24 @@ async function loginFunction() {
     if(resp.status === 200){
         console.log("You made it to the response.status === 200 part");
         document.getElementById("loginSection").innerText = "You have successfully logged in!";
-        window.location.href = "finManPage.html";
+        mosesPage();
     } else {
+        console.log("Ya dun goofed. Check your login credentials.");
         document.getElementById("loginSection").innerText = "Your login attempt has failed.";
         // This could possibly go to a Failure Page, which has a button that says, "take me back"
     }
-
 }
+
+// It's called "mosesPage" because it divides the users into two sections... get it?
+async function mosesPage(){
+    let data = await resp.JSON();   // this is the incoming data from our Tomcat server--it's a JSON string of our logged-in user's information.
+    console.log(data);              // curious to see what happens here. So far, nothing...?
+    window.location.href = "successPage";
+    let uId = data.userId;
+    sessionStorage.setItem("userId", userId);       // I could like th understand why this is a thing.
+    let uRole = data.UserRoles.userRoleId;
+}
+
+
+//         window.location.href = "employeePage.html";
+//         window.location.href = "finManPage.html";
